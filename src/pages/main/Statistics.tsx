310747ae40c1d7682cbc5d037743e7fad9cfa1a2
Seongseokwon <pam9411@naver.com>
2021-03-22 17:30:46 +0900

    ✨ feat/10/statistics_frame

---
+++ src/pages/main/Statistics.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Statistics() {
+  return (
+    <div>
+      <h2>파트너사 준비중 입니다.</h2>
+      <div>차트 들어가는 영역</div>
+    </div>
+  );
+}
+
+export default Statistics;
