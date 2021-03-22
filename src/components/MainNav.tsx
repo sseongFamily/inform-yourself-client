7601380d2dfe1199ae2e3a93a3f9186bd4b849d6
SungSeokMin <jkl154527@gmail.com>
2021-03-22 16:43:10 +0900

    ✨ feat/create main navigation

---
+++ src/components/MainNav.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function MainNav() {
+  // TODO: 비로그인시 board, letter, mypage 버튼
+  // TODO: 비로그인시 back, letter, myapge 버튼
+  // 비로그인 false , 로그인 true
+  const [isCheck, setIsCheck] = useState(false);
+
+  // console.log(window.location.href.slice(22) === 'infocard');
+
+  return (
+    <div>
+      <Link to="/infocard">
+        <i className="far fa-clipboard fa-3x" />
+      </Link>
+      <Link to="/">
+        <span>Inform Yourself</span>
+      </Link>
+      {isCheck ? (
+        'profile'
+      ) : (
+        <Link to="/mypage">
+          <i className="far fa-address-card fa-3x" />
+        </Link>
+      )}
+    </div>
+  );
+}
+
+export default MainNav;
