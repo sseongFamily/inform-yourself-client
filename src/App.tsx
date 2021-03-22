5c339ad089d6c58fde910e50737ec297b669a959
SungSeokMin <jkl154527@gmail.com>
2021-03-22 17:05:55 +0900

    ✨ feat/6/create_news_tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import MainNav from './components/MainNav';
+import News from './pages/main/News';
 
 function App() {
   return (
     <div>
       <MainNav />
+      <News />
     </div>
   );
 }
---
+++ src/pages/main/News.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import HotUser from '../../containers/main/HotUser';
+import NewUser from '../../containers/main/NewUser';
+
+function News() {
+  return (
+    // News Page
+    <div>
+      <NewUser />
+      <HotUser />
+    </div>
+  );
+}
+
+export default News;
