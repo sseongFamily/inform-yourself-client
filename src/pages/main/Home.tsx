8d363c09fe8503507b3c3da6699f208d4b0fe999
Seongseokwon <pam9411@naver.com>
2021-03-22 17:19:49 +0900

    ✨ feat/11/partner_company_frame

--- src/pages/main/Home.tsx
+++ src/pages/main/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Intro from './Intro';
+import News from './News';
+import PartnerCompany from './PartnerCompany';
 
 function Home() {
   return (
     <div>
       <Intro />
+      <News />
+      <PartnerCompany />
     </div>
   );
 }
