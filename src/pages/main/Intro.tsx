cedfde97b3382d967a9067b25e10e3dcbff7b52b
Seongseokwon <pam9411@naver.com>
2021-03-23 16:25:30 +0900

    ✨ feat/28/main_first_page_styling

--- src/pages/main/Intro.tsx
+++ src/pages/main/Intro.tsx
@@ -1,10 +1,49 @@
 import React from 'react';
-
+import SloganImage from '../../images/slogan_bg.svg';
 function Intro() {
   return (
-    <div>
-      <div>슬로건 들어갈 영역</div>
-      <div>gif 들어갈 영역</div>
+    <div
+      style={{
+        display: 'flex',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <div
+        style={{
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column-reverse',
+          width: '50%',
+          height: '60%',
+          margin: '0 5%',
+        }}
+      >
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            backgroundImage: `url(${SloganImage})`,
+            backgroundSize: 'contain',
+            backgroundPosition: 'center center',
+            backgroundRepeat: 'no-repeat',
+            opacity: '0.5',
+          }}
+        ></div>
+        <h2
+          style={{
+            alignItems: 'center',
+            textAlign: 'center',
+            fontSize: '36px',
+            color: '#black',
+            zIndex: 1,
+          }}
+        >
+          당신을 알리고 싶다면 지금 시작하세요 !
+        </h2>
+      </div>
+      <div style={{ width: '50%', height: '60%', margin: '0 5%' }}>gif 들어갈 영역</div>
     </div>
   );
 }
